91a7bc5f3161482bc9ae6b457384a333b2b1f5f7
Egor Lakomkin <egor.lakomkin@gmail.com>
2013-12-13 11:14:23 +0400

    initial commit

---
+++ src/com/apps4allhack/urinatest/Constants.java
@@ -0,0 +1,5 @@
+package com.apps4allhack.urinatest;
+public class Constants {
+	public final static String DEBUG_TAG = "com.apps4allhack.urinatest";
+
+}
---
+++ src/com/apps4allhack/urinatest/MakePhotoActivity.java
@@ -0,0 +1,87 @@
+package com.apps4allhack.urinatest;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.PictureCallback;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+public class MakePhotoActivity extends Activity {
+	private final static String DEBUG_TAG = "MakePhotoActivity";
+	
+	
+	 private Camera mCamera;
+	 private CameraPreview mPreview;
+
+	 private PictureCallback mPicture = new PictureCallback() {
+
+		    @Override
+		    public void onPictureTaken(byte[] data, Camera camera) {
+
+		       Log.i("MYAPP_PHOTO", "LEN_"+ String.valueOf(data.length));
+		    }
+		};
+	 
+	 
+	/** Check if this device has a camera */
+	private boolean checkCameraHardware(Context context) {
+	    if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+	        // this device has a camera
+	        return true;
+	    } else {
+	        // no camera on this device
+	        return false;
+	    }
+	}
+	
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);	
+	
+		
+		mCamera = getCameraInstance();
+
+	        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+        
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // get an image from the camera
+                    mCamera.takePicture(null, null, mPicture);
+                }
+            }
+        );
+	}
+
+
+}
