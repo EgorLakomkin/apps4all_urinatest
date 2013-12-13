fe77da8b755728ca108009f6623a100c60755251
Egor Lakomkin <egor.lakomkin@gmail.com>
2013-12-13 16:37:45 +0400

    server.py

---
+++ webservice/server.py
@@ -0,0 +1,22 @@
+#!-*- coding:utf-8 -*-
+from flask import Flask, render_template, jsonify, request
+
+import os
+
+@app.route('/statistics', methods=['GET'])
+def statistics():
+  pass
+
+@app.route('/last_analysis', methods=['GET'])
+def statistics():
+  pass
+
+  
+@app.route('/push_result', methods=['POST'])
+def statistics():
+  pass
+  
+  
+if __name__ == "__main__":
+  root_dirname, root_filename = os.path.split(os.path.abspath(__file__))
+  
