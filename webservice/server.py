4931c88d28a36a708eeddee955a90491ff76c0fa
Egor Lakomkin <egor.lakomkin@gmail.com>
2013-12-13 16:40:27 +0400

    templates

--- webservice/server.py
+++ webservice/server.py
@@ -20,3 +20,8 @@ def statistics():
 if __name__ == "__main__":
   root_dirname, root_filename = os.path.split(os.path.abspath(__file__))
   
+  port = int(sys.argv[1])
+  
+  app.run(host = '0.0.0.0', port = port, debug=True)
+  
+  
