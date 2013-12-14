6b613a81be0a26a6f2c57a6b115a1d601bf79e39
Egor Lakomkin <egor.lakomkin@gmail.com>
2013-12-14 08:25:07 +0400

    pepre

--- src/org/opencv/samples/tutorial1/ByteArrayPost.java
+++ src/org/opencv/samples/tutorial1/ByteArrayPost.java
@@ -8,16 +8,21 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ByteArrayBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
@@ -26,6 +31,7 @@ import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Base64;
 import android.util.Log;
 
 public class ByteArrayPost extends AsyncTask<String, String, String> {
@@ -46,8 +52,20 @@ public class ByteArrayPost extends AsyncTask<String, String, String> {
 		// TODO Auto-generated method stub
 
 		HttpClient httpClient = new DefaultHttpClient();
+		String base64 = Base64.encodeToString(array, Base64.DEFAULT);
+		
+		
 		HttpPost postRequest = new HttpPost(req_url);
-		postRequest.setEntity(new ByteArrayEntity(array));  
+		
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("image",base64));
+		
+		try {
+			postRequest.setEntity(new UrlEncodedFormEntity(params));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}  
 		try {
 			HttpResponse response = httpClient.execute(postRequest);
 			Log.v("MYAPP","OK");
